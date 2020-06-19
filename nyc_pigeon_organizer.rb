f2eabf910228c0775bb31498d8400efc988b6126
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-19 02:43:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
   #binding.pry
   value.each do |inner_key, names|
     names.each do |name|
-     binding.pry
+     #binding.pry
      if !final_array[name]
        final_array[name] = {}
      end
@@ -15,5 +15,8 @@ def nyc_pigeon_organizer(data)
   end
   final_array[name][key].push(inner_key)
   end
+end
+ #final_array
+ end
 binding.pry
 end
